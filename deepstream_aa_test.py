0fa3055fb61536298340ea02c7ccf3a632dbc763
aadamczak <adamadamczak123@gmail.com>
2023-12-12 18:36:09 +0000

    changed target filepath

--- deepstream_aa_test.py
+++ deepstream_aa_test.py
@@ -9,8 +9,12 @@ from common.bus_call import bus_call
 from IPython.display import Video
 import pyds
 from utils.probes import pgie_src_pad_buffer_probe
+from common.bus_call import bus_call
+from inspect import getsource
+
+
 config_path = "configs/config_infer_primary_yoloV8.txt"
-target_video_path="/opt/nvidia/deepstream/deepstream-6.3/samples/streams/sample_720p.h264"
+target_video_path="../../../../samples/streams/sample_720p.h264"
 PGIE_CLASS_ID_VEHICLE = 0
 PGIE_CLASS_ID_BICYCLE = 1
 PGIE_CLASS_ID_PERSON = 2
@@ -19,7 +23,7 @@ Gst.init(None)
 
 pipeline=Gst.Pipeline()
 print('Created pipeline')
-### CREATE GST ELEMENTS
+
 source=Gst.ElementFactory.make("filesrc", "file-source")
 source.set_property('location', target_video_path)
 
@@ -51,7 +55,7 @@ filesink=Gst.ElementFactory.make('filesink', 'filesink')
 filesink.set_property('location', 'output.mp4')
 filesink.set_property("sync", 1)
 print('Created elements')
-### ADD GST ELEMENTS TO PIPELINE
+
 pipeline.add(source)
 pipeline.add(h264parser)
 pipeline.add(decoder)
@@ -68,7 +72,7 @@ pipeline.add(filesink)
 print('Added elements to pipeline')
 
 
-### LINK  GST ELEMENTS
+
 source.link(h264parser)
 h264parser.link(decoder)
 
@@ -88,39 +92,23 @@ mux.link(filesink)
 print('Linked elements in pipeline')
 
 
-
-# DO NOT CHANGE THIS CELL
-# Declare list to hold count data
-obj_counts=[]
-
-# Define the Probe Function
-
 pgie_src_pad=pgie.get_static_pad('src')
 probe_id=pgie_src_pad.add_probe(Gst.PadProbeType.BUFFER, pgie_src_pad_buffer_probe)
 print('Attached probe')
 
-from common.bus_call import bus_call
-
-# Inspect the definition for bus_call
-from inspect import getsource
 print(getsource(bus_call))
-# DO NOT CHANGE THIS CELL
-# Create an event loop
+
 loop=GLib.MainLoop()
 
-# Feed GStreamer bus messages to loop
+
 bus=pipeline.get_bus()
 bus.add_signal_watch()
 bus.connect("message", bus_call, loop)
 print('Added bus message handler')
-# DO NOT CHANGE THIS CELL
-# Start play back and listen to events
 print("Starting pipeline")
 pipeline.set_state(Gst.State.PLAYING)
 try:
     loop.run()
 except:
     pass
-
-# Cleaning up as the pipeline comes to an end
 pipeline.set_state(Gst.State.NULL)

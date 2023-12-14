c8ec0eabf0a7c5ae3ec506481177e079dcaa5406
aadamczak <adamadamczak123@gmail.com>
2023-12-14 21:44:09 +0000

    add docker folder

---
+++ docker/run.sh
@@ -0,0 +1,23 @@
+xhost +local:root
+
+# BUILD THE IMAGE
+IMAGE="deepstream_yolo_py"
+CONTAINER="aa_DeepstreamYoloPy"
+OUTPUT_PATH="output.mp4"
+SCRIPT_NAME = "deepstream_aa_test.py"
+
+
+docker stop $CONTAINER || true && docker rm $CONTAINER || true
+
+sudo docker run -it \
+	--rm \
+	--net=host \
+	--runtime nvidia \
+	--privileged=true \
+	--volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
+	# -e DISPLAY \
+	-v /tmp/.X11-unix/:/tmp/.X11-unix \
+	--name CONTAINER \
+	$IMAGE \
+	python3 deepstream_aa_test.py
+
